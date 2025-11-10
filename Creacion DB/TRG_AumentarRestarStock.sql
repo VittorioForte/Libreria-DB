7113918128053f54ac28da677fad41f54edb6ddd
Vittorio Forte <164781730+VittorioForte@users.noreply.github.com>
2025-11-10 13:50:00 -0300

    Add files via upload

---
+++ Creacion DB/TRG_AumentarRestarStock.sql
@@ -0,0 +1,32 @@
+USE tp_sistema_gpb;
+GO
+
+CREATE TRIGGER trg_RestarStockLibros
+ON Prestamos
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Libros
+    SET Libros.Stock_Disponible = Libros.Stock_Disponible - 1
+    FROM Libros
+    INNER JOIN inserted i ON Libros.ID_Libro = i.ID_Libro;
+END;
+GO
+
+CREATE TRIGGER trg_AumentarStockLibros
+ON Prestamos
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON; 
+
+    UPDATE Libros
+    SET Libros.Stock_Disponible = Libros.Stock_Disponible + 1
+    FROM Libros
+    INNER JOIN inserted i ON Libros.ID_Libro = i.ID_Libro
+    INNER JOIN deleted d ON d.ID_Prestamo = i.ID_Prestamo
+    WHERE d.Fecha_Devolucion_Prevista IS NULL AND i.Fecha_Entrada IS NOT NULL;
+END;
+GO
