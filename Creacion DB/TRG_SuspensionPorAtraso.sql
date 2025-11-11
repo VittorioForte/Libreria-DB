249f86fac4ed44059a00984e8e8ce33c9aad3d08
Vittorio Forte <164781730+VittorioForte@users.noreply.github.com>
2025-11-11 17:59:25 -0300

    CORRECCION TRIGGER 2

--- Creacion DB/TRG_SuspensionPorAtraso.sql
+++ Creacion DB/TRG_SuspensionPorAtraso.sql
@@ -10,23 +10,42 @@ ADD
 GO
 
 --Se crea el trigger para actualizar las fechas de suspensión al insertar una sanción de tipo "Suspensión por Atraso"
--- La fecha de inicio será la fecha de sanción y la fecha de fin será 15 días después.
+--La fecha de inicio será la fecha de sanción y la fecha de fin será 15 días después.
 
-CREATE TRIGGER trg_SuspensionPorAtraso
-ON Sanciones
-AFTER INSERT
+CREATE TRIGGER trg_SancionPorRetraso
+ON dbo.Prestamos
+AFTER UPDATE
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    UPDATE s
-    SET
-        s.Fecha_InicioSuspension = i.Fecha_Sancion,
-        s.Fecha_FinSuspension = DATEADD(DAY, 15, i.Fecha_Sancion)
-    FROM Sanciones s
-    INNER JOIN inserted i ON s.ID_Sancion = i.ID_Sancion
-    INNER JOIN Prestamos p ON p.ID_Prestamo = i.ID_Prestamo
-    WHERE i.Tipo = 'Suspensión por Atraso';
+    DECLARE 
+        @ID_Prestamo INT,
+        @ID_Estudiante INT,
+        @FechaDevolucionPrevista DATE,
+        @FechaEntrada DATE;
 
+    -- Tomamos los datos del préstamo actualizado
+    SELECT 
+        @ID_Prestamo = i.ID_Prestamo,
+        @ID_Estudiante = i.ID_Estudiante,
+        @FechaDevolucionPrevista = i.Fecha_Devolucion_Prevista,
+        @FechaEntrada = i.Fecha_Entrada
+    FROM inserted i
+    INNER JOIN deleted d ON i.ID_Prestamo = d.ID_Prestamo
+    WHERE d.Fecha_Entrada IS NULL AND i.Fecha_Entrada IS NOT NULL;
+
+    -- Si la devolución es posterior a la prevista, se aplica sanción
+    IF @FechaEntrada >= @FechaDevolucionPrevista
+    BEGIN
+        INSERT INTO Sanciones (ID_Prestamo, Tipo, Fecha_Sancion)
+        VALUES (@ID_Prestamo, 'Retraso en devolución', GETDATE());
+
+        UPDATE Sanciones
+        SET 
+            Fecha_InicioSuspension = GETDATE(),
+            Fecha_FinSuspension = DATEADD(DAY, 15, GETDATE())
+        WHERE ID_Prestamo = @ID_Prestamo;
+    END
 END;
 GO
