0aaafc9391c808d7f3ef4eb84a987f11f45212d7
Vittorio Forte <164781730+VittorioForte@users.noreply.github.com>
2025-11-14 17:35:22 -0300

    Correccion

--- Creacion DB/TRG_AumentarRestarStock.sql
+++ Creacion DB/TRG_AumentarRestarStock.sql
@@ -20,13 +20,32 @@ ON Prestamos
 AFTER UPDATE
 AS
 BEGIN
-    SET NOCOUNT ON; 
+    SET NOCOUNT ON;
 
     UPDATE Libros
-    SET Libros.Stock_Disponible = Libros.Stock_Disponible + 1
+    SET Stock_Disponible = Stock_Disponible + 1
     FROM Libros
-    INNER JOIN inserted i ON Libros.ID_Libro = i.ID_Libro
-    INNER JOIN deleted d ON d.ID_Prestamo = i.ID_Prestamo
-    WHERE d.Fecha_Devolucion_Prevista IS NULL AND i.Fecha_Entrada IS NOT NULL;
+    INNER JOIN inserted i  ON Libros.ID_Libro = i.ID_Libro
+    INNER JOIN deleted d   ON d.ID_Prestamo  = i.ID_Prestamo
+    WHERE d.Fecha_Entrada IS NULL
+    AND i.Fecha_Entrada IS NOT NULL;
 END;
 GO
+
+-- Pruebas de los triggers
+
+--EXEC SP_RegistrarPrestamo
+--    @ID_Libro = 4,
+--    @ID_Estudiante = 21,
+--    @DiasDePrestamo = 15;
+--GO
+
+--EXEC SP_RegistrarDevolucion
+--    @ID_Prestamo = 1004;
+--GO
+
+
+--SELECT ID_Libro, Titulo, Stock_Disponible
+--FROM Libros
+--GO
+
