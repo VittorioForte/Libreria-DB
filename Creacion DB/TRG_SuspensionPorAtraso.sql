874224ee5425195f663c0960395d75a253fc0490
Vittorio Forte <164781730+VittorioForte@users.noreply.github.com>
2025-11-14 17:48:29 -0300

    Correcion

--- Creacion DB/TRG_SuspensionPorAtraso.sql
+++ Creacion DB/TRG_SuspensionPorAtraso.sql
@@ -1,51 +1,45 @@
-USE tp_sistema_gpb;
-GO
 
---Se agrega las columnas para las fechas de inicio y fin de suspensión en la tabla Sanciones
-
-ALTER TABLE Sanciones
-ADD 
-    Fecha_InicioSuspension DATE NULL,
-    Fecha_FinSuspension DATE NULL;
-GO
-
---Se crea el trigger para actualizar las fechas de suspensión al insertar una sanción de tipo "Suspensión por Atraso"
---La fecha de inicio será la fecha de sanción y la fecha de fin será 15 días después.
-
-CREATE TRIGGER trg_SancionPorRetraso
-ON dbo.Prestamos
+CREATE TRIGGER trg_SuspensionPorAtraso
+ON Prestamos
 AFTER UPDATE
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    DECLARE 
-        @ID_Prestamo INT,
-        @ID_Estudiante INT,
-        @FechaDevolucionPrevista DATE,
-        @FechaEntrada DATE;
-
-    -- Tomamos los datos del préstamo actualizado
-    SELECT 
-        @ID_Prestamo = i.ID_Prestamo,
-        @ID_Estudiante = i.ID_Estudiante,
-        @FechaDevolucionPrevista = i.Fecha_Devolucion_Prevista,
-        @FechaEntrada = i.Fecha_Entrada
+    INSERT INTO Sanciones (
+        ID_Prestamo,
+        Tipo,
+        Monto,
+        Fecha_Sancion,
+        Fecha_InicioSuspension,
+        Fecha_FinSuspension
+    )
+    SELECT
+        i.ID_Prestamo,                    
+        'Atraso en la devolución',        
+        0,                                 
+        i.Fecha_Entrada,                  
+        i.Fecha_Entrada,                  
+        DATEADD(DAY, 15, i.Fecha_Entrada) 
     FROM inserted i
-    INNER JOIN deleted d ON i.ID_Prestamo = d.ID_Prestamo
-    WHERE d.Fecha_Entrada IS NULL AND i.Fecha_Entrada IS NOT NULL;
-
-    -- Si la devolución es posterior a la prevista, se aplica sanción
-    IF @FechaEntrada >= @FechaDevolucionPrevista
-    BEGIN
-        INSERT INTO Sanciones (ID_Prestamo, Tipo, Fecha_Sancion)
-        VALUES (@ID_Prestamo, 'Retraso en devolución', GETDATE());
-
-        UPDATE Sanciones
-        SET 
-            Fecha_InicioSuspension = GETDATE(),
-            Fecha_FinSuspension = DATEADD(DAY, 15, GETDATE())
-        WHERE ID_Prestamo = @ID_Prestamo;
-    END
+    INNER JOIN deleted d 
+        ON d.ID_Prestamo = i.ID_Prestamo
+    WHERE 
+        d.Fecha_Entrada IS NULL               
+        AND i.Fecha_Entrada IS NOT NULL    
+        AND i.Fecha_Entrada > i.Fecha_Devolucion_Prevista; 
 END;
 GO
+
+-- Prueba del trigger
+
+--EXEC SP_RegistrarPrestamo 17, 1;
+
+--UPDATE Prestamos
+--SET Fecha_Devolucion_Prevista = '2024-06-01'
+--WHERE ID_Prestamo = (SELECT MAX(ID_Prestamo) FROM Prestamos);
+
+--EXEC SP_RegistrarDevolucion
+--    @ID_Prestamo = 1005;
+--GO
+
