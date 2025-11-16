2fbd56040da74b1c5a87c899b4beb9d41c4a78d5
Vittorio Forte <164781730+VittorioForte@users.noreply.github.com>
2025-11-16 18:49:48 -0300

    Add files via upload

--- Creacion DB/SP_RegistrarPrestamo.sql
+++ Creacion DB/SP_RegistrarPrestamo.sql
@@ -1,6 +1,3 @@
-USE tp_sistema_gpb;
-GO
-
 CREATE PROCEDURE SP_RegistrarPrestamo
     @ID_Estudiante INT,
     @ID_Libro INT,
@@ -13,14 +10,14 @@ BEGIN
         -- 1. Validar que el estudiante exista
         IF NOT EXISTS (SELECT 1 FROM Usuarios WHERE ID_Estudiante = @ID_Estudiante)
         BEGIN
-            RAISERROR('Error: El ID_Estudiante %d no existe.', 16, 1, @ID_Estudiante);
+            RAISERROR('Error: El ID_Estudiante id no existe.', 16, 1, @ID_Estudiante);
             RETURN;
         END
 
         -- 2. Validar que el libro exista
         IF NOT EXISTS (SELECT 1 FROM Libros WHERE ID_Libro = @ID_Libro)
         BEGIN
-            RAISERROR('Error: El ID_Libro %d no existe.', 16, 1, @ID_Libro);
+            RAISERROR('Error: El ID_Libro id no existe.', 16, 1, @ID_Libro);
             RETURN;
         END
 
@@ -30,21 +27,38 @@ BEGIN
         
         IF (@StockActual <= 0)
         BEGIN
-            RAISERROR('Error: No hay stock disponible para el libro ID %d.', 16, 1, @ID_Libro);
+            RAISERROR('Error: No hay stock disponible para el libro ID.', 16, 1, @ID_Libro);
             RETURN;
         END
 
         -- 4. Validar que el estudiante no tenga préstamos vencidos
         IF EXISTS (SELECT 1 FROM Prestamos 
-                   WHERE ID_Estudiante = @ID_Estudiante 
-                   AND Fecha_Entrada IS NULL -- Préstamo activo
-                   AND Fecha_Devolucion_Prevista < GETDATE()) -- Vencido
+            WHERE ID_Estudiante = @ID_Estudiante 
+            AND Fecha_Entrada IS NULL -- Préstamo activo
+            AND Fecha_Devolucion_Prevista < GETDATE()) -- Vencido
         BEGIN
             RAISERROR('Error: El estudiante tiene préstamos vencidos. No puede retirar nuevos libros.', 16, 1);
             RETURN;
         END
 
         -- --- FIN VALIDACIONES ---
+
+        --- --- IF TIENE UNA SUSPENSION ACTIVA, NO DEJA HACER PRESTAMO --- ---
+
+                IF EXISTS (
+            SELECT 1
+            FROM Sanciones
+            WHERE ID_Prestamo IN (
+                SELECT ID_Prestamo 
+                FROM Prestamos 
+                WHERE ID_Estudiante = @ID_Estudiante
+            )
+            AND Fecha_FinSuspension >= CAST(GETDATE() AS DATE)
+        )
+        BEGIN
+            RAISERROR('Error: El estudiante está suspendido y no puede retirar libros.', 16, 1);
+            RETURN;
+        END
 
         -- 5. Si todo está OK, registrar el préstamo
         INSERT INTO Prestamos (
