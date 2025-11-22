9bfb4f0f780d2010ca00023c78f00ef85929c89e
Vittorio Forte <164781730+VittorioForte@users.noreply.github.com>
2025-11-22 11:38:15 -0300

    Correccion error

--- Creacion DB/SP_RegistrarDevolucion.sql
+++ Creacion DB/SP_RegistrarDevolucion.sql
@@ -11,44 +11,34 @@ BEGIN
         
         -- --- VALIDACIONES ---
 
-        -- 1. Validar que el préstamo exista
         IF NOT EXISTS (SELECT 1 FROM Prestamos WHERE ID_Prestamo = @ID_Prestamo)
         BEGIN
-            RAISERROR('Error: El ID_Prestamo %d no existe.', 16, 1, @ID_Prestamo);
+            RAISERROR('Error: El ID_Prestamo id no existe.', 16, 1, @ID_Prestamo);
             RETURN;
         END
 
-        -- 2. Validar que el préstamo NO haya sido devuelto ya
         IF EXISTS (SELECT 1 FROM Prestamos WHERE ID_Prestamo = @ID_Prestamo AND Fecha_Entrada IS NOT NULL)
         BEGIN
-            RAISERROR('Error: Este préstamo ya fue devuelto anteriormente.', 16, 1);
+            RAISERROR('Error: Este prestamo ya fue devuelto anteriormente.', 16, 1);
             RETURN;
         END
 
         -- --- FIN VALIDACIONES ---
 
-        -- Obtenemos la fecha en que DEBÍA ser devuelto
         DECLARE @FechaPrevista DATE;
         SELECT @FechaPrevista = Fecha_Devolucion_Prevista 
         FROM Prestamos 
         WHERE ID_Prestamo = @ID_Prestamo;
 
-        -- 3. Actualizar el préstamo marcándolo como devuelto hoy
         UPDATE Prestamos
         SET Fecha_Entrada = GETDATE()
         WHERE ID_Prestamo = @ID_Prestamo;
 
-        PRINT 'Devolución registrada para el préstamo ID %d.';
+        PRINT 'Devolucion registrada para el prestamo ID id.';
 
-        -- 4. Lógica de Sanción: ¿Lo devolvió tarde?
         IF (GETDATE() > @FechaPrevista)
         BEGIN
-            -- El préstamo está vencido. Insertamos una sanción.
-            -- (El monto es un ejemplo, puedes ajustarlo)
-            INSERT INTO Sanciones (ID_Prestamo, Tipo, Monto)
-            VALUES (@ID_Prestamo, 'Devolución Tarde', 250.50);
-            
-            PRINT 'Se ha generado una sanción por devolución tardía.';
+            PRINT 'Se ha generado una sancion por devolucion tardia.';
         END
 
     END TRY
@@ -57,3 +47,4 @@ BEGIN
     END CATCH
 END;
 GO
+
